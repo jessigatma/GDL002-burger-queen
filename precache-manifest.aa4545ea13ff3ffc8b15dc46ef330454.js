8641754c33cc88e068c044b5b09c72da0036b444
jessigatma <jessgat@hotmail.com>
2019-05-28 09:13:53 -0500

    Updates

--- precache-manifest.aa4545ea13ff3ffc8b15dc46ef330454.js
+++ precache-manifest.aa4545ea13ff3ffc8b15dc46ef330454.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3998cbb1a22eedb78ec9b42030a69dc8",
+    "revision": "b7c25ecbaa9f6c7b290ff1d68f7f7ec6",
     "url": "/gdl002-burger-queen/index.html"
   },
   {
-    "revision": "f11fdfc1b374498a96c4",
+    "revision": "09140d5e1c1793aa3f7f",
     "url": "/gdl002-burger-queen/static/css/main.a062ac0e.chunk.css"
   },
   {
-    "revision": "f9cc5a70bf214b4c9a88",
-    "url": "/gdl002-burger-queen/static/js/2.a21ac216.chunk.js"
+    "revision": "6fb77869ba4f8f720587",
+    "url": "/gdl002-burger-queen/static/js/2.b6c16f8d.chunk.js"
   },
   {
-    "revision": "f11fdfc1b374498a96c4",
-    "url": "/gdl002-burger-queen/static/js/main.3730a42f.chunk.js"
+    "revision": "09140d5e1c1793aa3f7f",
+    "url": "/gdl002-burger-queen/static/js/main.19390487.chunk.js"
   },
   {
     "revision": "abf1e5c4314e6441e9d2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gdl002-burger-queen/precache-manifest.245248e0c59b4fe80e067eadf3eb8ebf.js"
+  "/gdl002-burger-queen/precache-manifest.aa4545ea13ff3ffc8b15dc46ef330454.js"
 );
 
 self.addEventListener('message', (event) => {
