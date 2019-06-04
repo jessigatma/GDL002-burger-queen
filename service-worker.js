2f418e5c84349badebb58bedf08de7e0c76c8040
jessigatma <jessgat@hotmail.com>
2019-06-04 12:48:02 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gdl002-burger-queen/precache-manifest.8832bacc3e7963fbb10eb6e0a53af413.js"
+  "/gdl002-burger-queen/precache-manifest.772208e8bb39bdd63f1717492d22a63e.js"
 );
 
 self.addEventListener('message', (event) => {
