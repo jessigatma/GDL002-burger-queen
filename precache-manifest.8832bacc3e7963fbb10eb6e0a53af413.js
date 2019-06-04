2abcf974c7d087c5e83b6064210a02e3e4200a74
jessigatma <jessgat@hotmail.com>
2019-06-04 12:50:56 -0500

    Updates

--- precache-manifest.8832bacc3e7963fbb10eb6e0a53af413.js
+++ precache-manifest.8832bacc3e7963fbb10eb6e0a53af413.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8869b1ceb471bc21309dae7e63daa781",
+    "revision": "93fae881cabc41f6a59a5a6f3dc3988c",
     "url": "/gdl002-burger-queen/index.html"
   },
   {
-    "revision": "41b3425834b1480f2c37",
+    "revision": "791c65b7b366088b4c88",
     "url": "/gdl002-burger-queen/static/css/main.a062ac0e.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gdl002-burger-queen/static/js/2.b6c16f8d.chunk.js"
   },
   {
-    "revision": "41b3425834b1480f2c37",
-    "url": "/gdl002-burger-queen/static/js/main.398e6173.chunk.js"
+    "revision": "791c65b7b366088b4c88",
+    "url": "/gdl002-burger-queen/static/js/main.9bb8baad.chunk.js"
   },
   {
     "revision": "abf1e5c4314e6441e9d2",
